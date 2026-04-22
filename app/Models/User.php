91b00fca8054eb0b4ae50d4c6b8fa5d88f37dbbc
Md Shakibur Rahman <shakibur.rahman.fahim@gmail.com>
2026-04-22 21:24:46 +0600

    user has one cart

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->role === 'admin';
     }
+
+    public function cart(): HasOne
+    {
+        return $this->hasOne(Cart::class);
+    }
 }
